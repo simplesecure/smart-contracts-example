4fe6062e7be73edf10f9f1bf5cf85aa9840d4390
polluterofminds <justin@graphitedocs.com>
2019-09-11 13:02:35 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,139 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const port = 5000;
+
+const ethers = require('ethers');
+let provider = ethers.getDefaultProvider('ropsten');
+let url = "http://127.0.0.1:7545";
+let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+let address = "0xe9CF9486ECf63bdA487B64698085A51392f42081";
+let privateKey = "8192c7840f4ad34777acb0e89f7b4e1a134986f9ec09e57e6f3414dd813843f6";
+
+
+app.post('/ganache/deployContract', async (req, res) => {
+  const { abi, bytecode } = req.body;
+  //For the local Ganache blockchain
+  let wallet = new ethers.Wallet(privateKey, customHttpProvider);
+  customHttpProvider.getBalance(address).then((balance) => {
+
+    // balance is a BigNumber (in wei); format is as a sting (in ether)
+    let etherString = ethers.utils.formatEther(balance);
+
+    console.log("Balance: " + etherString);
+  });
+  let factory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+  // Notice we pass in "Hello World" as the parameter to the constructor
+  let contract = await factory.deploy();
+
+  // The address the Contract WILL have once mined
+  // See: https://ropsten.etherscan.io/address/0x2bd9aaa2953f988153c8629926d22a6a5f69b14e
+  console.log(contract.address);
+  // "0x2bD9aAa2953F988153c8629926D22A6a5F69b14E"
+
+  // The transaction that was sent to the network to deploy the Contract
+  // See: https://ropsten.etherscan.io/tx/0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51
+  console.log(contract.deployTransaction.hash);
+  // "0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51"
+
+  // The contract is NOT deployed yet; we must wait until it is mined
+  await contract.deployed()
+  res.send({message: "Contract deployed", address: contract.address, hash: contract.deployTransaction.hash });
+});
+
+app.post('/ropsten/deployContract', async (req, res) => {
+  const { abi, bytecode } = req.body;
+  let mnemonic = "day laundry wet frog census letter verify toe try biology love decrease";
+  let initwallet = new ethers.Wallet.fromMnemonic(mnemonic);
+  const ropPrivKey = initwallet.signingKey.keyPair.privateKey;
+  let ropWallet = new ethers.Wallet(ropPrivKey, provider);
+  let ropAddress = ropWallet.signingKey.address;
+  
+  provider.getBalance(ropAddress).then((balance) => {
+  
+    // balance is a BigNumber (in wei); format is as a sting (in ether)
+    let etherString = ethers.utils.formatEther(balance);
+  
+    console.log("Balance: " + etherString);
+  });
+  // Create an instance of a Contract Factory
+  let factory = new ethers.ContractFactory(abi, bytecode, ropWallet);
+
+  // Notice we pass in "Hello World" as the parameter to the constructor
+  let contract = await factory.deploy();
+
+  // The address the Contract WILL have once mined
+  // See: https://ropsten.etherscan.io/address/0x2bd9aaa2953f988153c8629926d22a6a5f69b14e
+  console.log(contract.address);
+  // "0x2bD9aAa2953F988153c8629926D22A6a5F69b14E"
+
+  // The transaction that was sent to the network to deploy the Contract
+  // See: https://ropsten.etherscan.io/tx/0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51
+  console.log(contract.deployTransaction.hash);
+  // "0x159b76843662a15bd67e482dcfbee55e8e44efad26c5a614245e12a00d4b1a51"
+
+  // The contract is NOT deployed yet; we must wait until it is mined
+  await contract.deployed()
+
+  // Done! The contract is deployed.
+  res.send({message: "Contract deployed", address: contract.address, hash: contract.deployTransaction.hash });
+});
+
+app.post('/ganache/fetchContract', async (req, res) => {
+  const { abi, contractAddress } = req.body;
+  let contract = await new ethers.Contract(contractAddress, abi, customHttpProvider);
+  // let tasks = await contract.tasks(1);
+  res.send(contract);
+});
+
+app.post('/ropsten/fetchContract', async (req, res) => {
+  const { abi, ropContractAddress } = req.body;
+  let contract = new ethers.Contract(ropContractAddress, abi, provider);
+  let tasks = await contract.tasks(1);
+  res.send(tasks);
+});
+
+app.post('/sendTx', async (req, res) => {
+  const { abi, contractAddress, updates, network } = req.body;
+  let wallet;
+
+  if(network === "local") {
+    wallet = new ethers.Wallet(privateKey, customHttpProvider);
+  } else if(network === "ropsten") {
+    let mnemonic = "day laundry wet frog census letter verify toe try biology love decrease";
+    let initwallet = new ethers.Wallet.fromMnemonic(mnemonic);
+    const ropPrivKey = initwallet.signingKey.keyPair.privateKey;
+    wallet = new ethers.Wallet(ropPrivKey, provider);
+  }
+
+  let contract = await new ethers.Contract(contractAddress, abi, wallet);
+  const updateFunction = updates.functionName;
+  const updateValue = updates.value;
+
+  let updatedContract = await contract[updateFunction](updateValue);
+  res.send(updatedContract);
+});
+
+app.post('/estimateGas', async (req, res) => {
+  const { contractAddress, network } = req.body;
+  let estimate;
+  const tx = {
+    to: contractAddress, 
+    data: "0x"
+  }
+  if(network === "local") {
+    estimate = await customHttpProvider.estimateGas(tx);
+  } else if(network === "ropsten") {
+    estimate = await provider.estimateGas(tx)
+  }
+
+  res.send(JSON.stringify(estimate.toNumber()));
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,185 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { tsArrayType } from '@babel/types';
+const ethers = require('ethers');
+const request = require('request-promise');
+let bytecode = require('./contracts/TodoList.json').bytecode;
+let abi = require('./contracts/TodoList.json').abi;
+let contractAddress = "0xEC80Dc659BBea656E6bA8FB15aE488c3cd74e812";
+let ropContractAddress = "0x38FbF13BbF1bc9abaaf2ece13aA1Aeb1b24d7a35";
+let headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' };
+let url = "http://127.0.0.1:7545";
+let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [], 
+      gas: 0,
+      showGas: false, 
+      loading: true, 
+      newTaskContent: "", 
+      payload: {}
+    }
+  }
+
+  componentDidMount() {
+    this.fetchContract();
+  }
+
+  estimateGas = (data) => {
+    let payload = data;
+    const options = { url: 'http://localhost:5000/estimateGas', method: 'POST', headers: headers, body: JSON.stringify(payload) };
+    return request(options)
+    .then(async (body) => {
+      // POST succeeded...
+      const gas = JSON.parse(body);
+      this.setState({ gas, showGas: true });
+    })
+    .catch(error => {
+      // POST failed...
+      console.log('ERROR: ', error)
+    });
+  }
+
+  deployContract = async() => {
+    //this needs to happen on the server
+    const payload = {
+      abi, 
+      bytecode
+    }
+  
+    const options = { url: 'http://localhost:5000/ganache/deployContract', method: 'POST', headers: headers, body: JSON.stringify(payload) };
+    return request(options)
+    .then(async (body) => {
+      // POST succeeded...
+      let contract = JSON.parse(body);
+      console.log(contract);
+    })
+    .catch(error => {
+      // POST failed...
+      console.log('ERROR: ', error)
+    });
+  }
+  
+  fetchContract = async () => {
+    let tasksArr = [];
+    //This can happen client side as no gas is spent
+    let contract = await new ethers.Contract(contractAddress, abi, customHttpProvider);
+    let taskCountRaw = await contract.taskCount();
+    const taskCount = taskCountRaw.toNumber();
+    console.log(taskCount);
+    var i;
+    for (i = 1; i < taskCount+1; i++) {
+      let task = await contract.tasks(i);
+      const taskObj = {
+        id: task[0].toNumber(),
+        content: task[1],
+        completed: task[2]
+      }
+      console.log(taskObj)
+      tasksArr.push(taskObj);
+      this.setState({tasks: tasksArr})
+    } 
+    this.setState({ loading: false });
+  }
+
+  newTask = async () => {
+    const { newTaskContent } = this.state;
+    const updates = {
+      functionName: "createTask",
+      value: newTaskContent
+    }
+    const payload = {
+      abi, 
+      contractAddress, 
+      updates, 
+      network: "local"
+    }
+
+    this.setState({ payload });
+    this.estimateGas(payload);
+  }
+
+  toggleTask = (id) => {
+    const { tasks } = this.state;
+    let thisTask = tasks[id];
+    thisTask.completed = !thisTask.completed;
+    const updates = {
+      functionName: "toggleCompleted",
+      value: id
+    }
+    const payload = {
+      abi, 
+      contractAddress, 
+      updates, 
+      network: "local"
+    }
+
+    this.setState({ payload });
+    this.estimateGas(payload);
+  }
+
+  approveTransaction = () => {
+    const { payload } = this.state;
+    this.setState({ newTaskContent: "", payload: {}, gas: 0, showGas: false, loading: true });
+    const options = { url: 'http://localhost:5000/sendTx', method: 'POST', headers: headers, body: JSON.stringify(payload) };
+    return request(options)
+    .then(async (body) => {
+      // POST succeeded...
+      console.log(JSON.parse(body));
+      this.fetchContract();
+    })
+    .catch(error => {
+      // POST failed...
+      this.setState({ loading: false });
+      console.log('ERROR: ', error);
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({newTaskContent: e.target.value })
+  }
+
+  render() {
+    const { tasks, gas, showGas, loading, newTaskContent } = this.state;
+    return (
+      <div>
+        {
+          loading ? 
+          <div>
+            <h1>Fetching tasks...</h1>
+          </div> : 
+          showGas ? 
+          <div>
+            <p>This transaction requires {gas} gas. Approve transaction?</p>
+            <button onClick={this.approveTransaction}>Approve</button>
+            <button onClick={this.rejectTransaction}>Reject</button>
+          </div> : 
+          <div>
+          <button onClick={this.deployContract}>Deploy Contract</button>
+          <button onClick={this.fetchContract}>Fetch Contract</button>
+            {
+              tasks.map((task) => {
+                return (
+                  <div key={task.id}>
+                    <p><input type="checkbox" onChange={() => this.toggleTask(task.id)} checked={task.completed} /> <span style={task.completed ? {textDecoration: "strikethrough"} : {textDecortaion: "none"}}>{task.content}</span></p>
+                  </div>
+                )
+              })
+            }
+            <div>
+              <input type="text" value={newTaskContent} onChange={this.handleChange} placeholder="new task" />
+              <button onClick={this.newTask}>New Task</button>
+            </div>
+          </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
